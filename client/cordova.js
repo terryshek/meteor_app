1a3bed00e034e640ec24c0fc263f3300cef84199
Terry <terry@somday.com>
2015-03-05 17:24:46 +0800

    meteor

---
+++ client/cordova.js
@@ -0,0 +1,7 @@
+Meteor.startup(function() {
+    navigator.geolocation.getCurrentPosition(success);
+});
+success = function(position) {
+    Session.set('location', position);
+    Meteor.call('fetchNearbyLocations', position.coords)
+}
---
+++ server/index.js
@@ -0,0 +1,2 @@
+
+Places._ensureIndex({'loc.coordinates':'2dsphere'});
